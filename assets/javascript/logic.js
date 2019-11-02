0b2ecf8c9a48be82da25cd05a572fae8b875fc35
Maddie <madeleinecgribble@gmail.com>
2019-11-02 10:37:25 +0800

    Basic JS outline added

---
+++ assets/javascript/logic.js
@@ -0,0 +1,16 @@
+let userAlias = "";
+let opponentAlias = "";
+let wins = 0;
+let losses = 0;
+
+let game = gameCode();
+
+let gameCode = function() {
+  // Game functions go here.
+};
+
+let userScreen = userScreenCode();
+
+let userScreenCode = function() {
+  // UI change functions go here.
+};
