76a4fe088b5f14a83aef03964c2914c233cf8e34
Maddie <madeleinecgribble@gmail.com>
2019-11-03 12:35:02 +0800

    Added player object and separate section for database functions

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,4 @@
+// Actual game code.
 let userAlias = "";
 let opponentAlias = "";
 let wins = 0;
@@ -9,8 +10,24 @@ let gameCode = function() {
   // Game functions go here.
 };
 
+// User interface.
 let userScreen = userScreenCode();
 
 let userScreenCode = function() {
   // UI change functions go here.
 };
+
+// Database code.
+let playerObject = {
+  id: "string",
+  wins: 0,
+  losses: 0,
+  alias: "string",
+  currentSelection: "string: paper | scissors | rock"
+};
+
+let databaseModify = databaseModifyCode();
+
+let databaseModifyCode = function() {
+  // Database functions go here.
+}
